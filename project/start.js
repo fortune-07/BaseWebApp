0db03dc33b1f39696cc78764cb6d58df9d4eb63a
fortune-07 <fortuneniguel@gmail.com>
2020-03-16 12:49:22 +0300

    made changes

--- project/start.js
+++ project/start.js
@@ -25,6 +25,10 @@ app.get('/contact', function (request, response) {
   response.render('pages/contact');
 });
 
+app.get('/my account', function (request, response) {
+  response.render('pages/my account');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
