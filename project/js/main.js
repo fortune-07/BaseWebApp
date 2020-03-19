6a250a9eeddf52818b345450771b4a7dd105a67f
fortune-07 <fortuneniguel@gmail.com>
2020-03-19 20:13:22 +0300

    made changes

--- project/js/main.js
+++ project/js/main.js
@@ -1,38 +1,25 @@
-function handleRegister(){
+function handleSignUp(){
   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    console.log(email.password);
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-    });
-  }); 
+    // ...
+  });
 }
 
-function handleLoginFormSubmit(){
-  var postUsername = $("uname").val();
-  var postPassword = $("psw").val();
-  console.log(postUsername);
+function handleSignIn(){
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
 }
 
-function addMessage(postTitle,postBody){
-  var postData = {
-    title: postTitle,
-    body: postBody
-  }
-
-  var database = firebase.database().ref("posts");
-
-  var newPostRef = database.push();
-  newPostRef.set(postData);
-}
-
-function handleMessageFormSubmit(){
-  var postTitle = $("post-title").val();
-  var postBody = $("post-body").val();
-  addMessage(postTitle,postBody);
+function handleSignOut(){
+  firebase.auth().signOut().then(function () {
+    // Sign-out successful.
+  }).catch(function (error) {
+    // An error happened.
+  });
 }
