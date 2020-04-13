c1dc1f2fd5316c532a5626a176ee0db39587df7e
fortune-07 <fortuneniguel@gmail.com>
2020-04-13 13:00:08 +0300

    made changes

--- project/js/main.js
+++ project/js/main.js
@@ -23,3 +23,14 @@ function handleSignOut(){
     // An error happened.
   });
 }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHmGJRvkvkOMInOFlq8-jupqKiVIyS3zg",
+  authDomain: "fortune-20b4b.firebaseapp.com",
+  databaseURL: "https://fortune-20b4b.firebaseio.com",
+  projectId: "fortune-20b4b",
+  storageBucket: "fortune-20b4b.appspot.com",
+  messagingSenderId: "954226282245",
+  appId: "1:954226282245:web:62c27a7231f9cc6755b16c",
+  measurementId: "G-0W60Y84PSF"
+};
