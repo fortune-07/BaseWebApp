22b66fac50662407c87c886cb7513ffcb6fae647
fortune-07 <fortuneniguel@gmail.com>
2020-07-06 13:24:54 +0300

    made changes

--- project/js/main.js
+++ project/js/main.js
@@ -41,6 +41,16 @@ function signInWithEmailAndPassword(){
   });
 }
 
+function createUserWithEmailAndPassword(){
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    alert("signed Up");
+  });
+}
+
 function handleSignIn(){
   var provider = new firebase.auth.GoogleAuthProvider();
 
