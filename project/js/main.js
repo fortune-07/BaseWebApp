0bbc5e9cbb79d2805e9e89ec464c18368a774283
fortune-07 <fortuneniguel@gmail.com>
2020-07-08 17:13:33 +0300

    made changes

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,3 @@
-let carts = document.querySelectorAll('.add-cart');
-
-for (let i=0; i < carts.length; i++) {
-  carts[i].addEventListener('click', () => {
-    cartNumbers();
-  })
-} 
-
 var firebaseConfig = {
   apiKey: "AIzaSyD-pRgQz1p_tlE00KtC4HvwBnM575dExqQ",
   authDomain: "mwagambo-cfa3a.firebaseapp.com",
@@ -64,66 +56,6 @@ function login(){
 
 function logout(){
   firebase.auth().signOut();
-}
-
-const auth = firebase.auth();
-
-function signIn(){
-  var email = document.getElementById("email");
-  var password = document.getElementById("password");
-
-  const promise = auth.createUserWithEmailAndPassword(email.value. password.value);
-  promise.catch(e => alert(e.maessage))
-
-  alert("signed Up");
-}
-
-function signInWithEmailAndPassword(){
-  firebase.auth().signUnWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
-}
-
-function createUserWithEmailAndPassword(){
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-    alert("signed Up");
-  });
-}
-
-function handleSignIn(){
-  var provider = new firebase.auth.GoogleAuthProvider();
-
-  firebase.auth().signInWithPopup(provider).then(function (result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    console.log(user.email);
-  }).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-}
-
-function handleSignOut(){
-  firebase.auth().signOut().then(function () {
-    // Sign-out successful.
-  }).catch(function (error) {
-    // An error happened.
-  });
 }
 
 function openNav() {
