00f85ef55aac021546a1b2a06e365be6604898b3
fortune-07 <fortuneniguel@gmail.com>
2020-08-10 15:53:07 +0300

    made changes

--- project/js/main.js
+++ project/js/main.js
@@ -81,7 +81,7 @@ document.getElementById('login-account');
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("maincontent").style.marginLeft = "0";
+  document.getElementById("maincontent").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
