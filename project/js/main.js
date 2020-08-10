8b224013ea756c08c318948391bcf7e41e613a8a
fortune-07 <fortuneniguel@gmail.com>
2020-08-10 13:01:50 +0300

    made changes

--- project/js/main.js
+++ project/js/main.js
@@ -80,9 +80,21 @@ document.getElementById('login-account');
 ;
 
 function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("maincontent").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("maincontent").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
+
+/*function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-}    
+}*/  
