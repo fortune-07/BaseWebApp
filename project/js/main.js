afa37eaa00997a544c99c929d1b08a17012dc310
fortune-07 <fortuneniguel@gmail.com>
2020-08-12 17:15:35 +0300

    made changes

--- project/js/main.js
+++ project/js/main.js
@@ -81,13 +81,13 @@ document.getElementById('login-account');
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
-  document.getElementById("maincontent").style.marginLeft = "300px";
+  /*document.getElementById("maincontent").style.marginLeft = "300px";*/
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("maincontent").style.marginLeft = "0";
+  /*document.getElementById("maincontent").style.marginLeft = "0";*/
   document.body.style.backgroundColor = "white";
 }
 
